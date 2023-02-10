b27f786f4a2b143719f7dba31768a18ffd094ca6
tanmoypaul1005 <tanmoypaul1005@gmail.com>
2023-02-11 00:48:22 +0600

    ok

--- output/script.js
+++ output/script.js
@@ -1,1 +1,7 @@
-console.log("uguihihd")
+"use strict";
+function add(a, b) {
+    return a + b;
+}
+console.log(add(2, 6));
+const a = ["a", "b"];
+a.push("1");
---
+++ src/script.ts
@@ -0,0 +1,7 @@
+function add(a: number, b: number): number {
+    return a + b;
+}
+console.log(add(2, 6));
+
+const a=["a","b"];
+a.push("1")
