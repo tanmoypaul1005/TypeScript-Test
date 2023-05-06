ab7a5b88bff0bc68147616d8670164d755885eab
Tanmoy paul <84624966+tanmoypaul1005@users.noreply.github.com>
2023-05-06 12:39:36 +0600

    ok

--- output/script.js
+++ output/script.js
@@ -1,9 +1,14 @@
 "use strict";
+// function
 function multiple(a, b) {
     return a + b;
 }
 console.log(multiple(1, 2));
-let array;
-array = [1, 2, 3];
+//array
+let array = [];
+array = [1, 2, 3, "kk"];
+//variable
 let a;
 let b;
+//OBJECT
+let c;
