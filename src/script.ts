b1ec3ca02f35822054561f582ee0092777f56852
Tanmoy paul <84624966+tanmoypaul1005@users.noreply.github.com>
2023-05-06 14:05:39 +0600

    ok

--- src/script.ts
+++ src/script.ts
@@ -2,7 +2,7 @@
 
 let multiple: Function
 
-multiple = (a: number, b: number, c?: string, d: number = 10): number => {
+multiple = (a: number, b: number, c?: string|number, d: number = 10): number => {
     return a + b
 }
 console.log(multiple(1, 2))
