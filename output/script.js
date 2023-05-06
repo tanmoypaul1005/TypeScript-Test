c1603f04117193699daf661b5f93e4fead66b7cb
Tanmoy paul <84624966+tanmoypaul1005@users.noreply.github.com>
2023-05-06 13:28:45 +0600

    ok

--- output/script.js
+++ output/script.js
@@ -1,19 +1,7 @@
 "use strict";
 // function
-function multiple(a, b) {
+let multiple;
+multiple = (a, b) => {
     return a + b;
-}
-console.log(multiple(1, 2));
-//array
-let array = [];
-array = [1, 2, 3, "kk"];
-//variable
-let a;
-let b;
-let d;
-//OBJECT
-let c;
-c = {
-    name: "kk",
-    age: 1
 };
+console.log(multiple(1, 2));
--- src/script.ts
+++ src/script.ts
@@ -1,27 +1,8 @@
 // function
-function multiple(a: number, b: number) {
+
+let multiple :Function
+
+multiple=(a: number, b: number)=> {
     return a + b
 }
 console.log(multiple(1, 2))
-
-//array
-let array: (number | string)[] = []
-array = [1, 2, 3, "kk"]
-
-//variable
-let a: string;
-let b: number | string;
-let d:any
-
-
-//OBJECT
-let c: {
-    name: string
-    age: number
-}
-
-c = {
-    name: "kk",
-    age: 1
-}
-
