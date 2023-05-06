2bd1397f0e96ba9df60bfec41ebd5d30b30515b8
Tanmoy paul <84624966+tanmoypaul1005@users.noreply.github.com>
2023-05-06 14:07:02 +0600

    ok

--- output/script.js
+++ output/script.js
@@ -5,3 +5,6 @@ multiple = (a, b, c, d = 10) => {
     return a + b;
 };
 console.log(multiple(1, 2));
+const multiple2 = (a, b, c) => {
+    return a + b;
+};
--- src/script.ts
+++ src/script.ts
@@ -6,3 +6,7 @@ multiple = (a: number, b: number, c?: string|number, d: number = 10): number => 
     return a + b
 }
 console.log(multiple(1, 2))
+
+const multiple2 = (a: number, b: number, c: {name:string ,age:number}): number => {
+    return a + b
+}
