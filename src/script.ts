e1bda1e4cdd00f3a0e307adb5677e16271627454
Tanmoy paul <84624966+tanmoypaul1005@users.noreply.github.com>
2023-05-07 21:32:05 +0600

    ok

--- src/script.ts
+++ src/script.ts
@@ -1,36 +1,3 @@
-// function
-type stringOrNumber = string | number
+enum RType {Success = 'success',Error = 'error',}
 
-let multiple: Function
-
-multiple = (a: number, b: number, c?: stringOrNumber, d: number = 10): number => {
-    return a + b
-}
-console.log(multiple(1, 2))
-
-const multiple2 = (a: number, b: number, c: { name: string, age: number, d: stringOrNumber }): number => {
-    return a + b
-}
-
-let calculation: (x: number, y: number, type: string) => number
-
-calculation = (a: number, b: number, type: string) => {
-    if (type === 'add') {
-        return a + b
-    } else {
-        return a - b
-    }
-}
-console.log(calculation(1, 2, 'add'))
-
-let userDetails: (userInfo: {
-    name: string,
-    age: number
-}) => string
-
-userDetails = (userInfo: {
-    name: string,
-    age: number
-}) => {
-    return userInfo.name
-}
+enum RType2 {Success ,Error ,}
