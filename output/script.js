58b5ddc8c8f44f160361561de488abf6e240c032
Tanmoy paul <84624966+tanmoypaul1005@users.noreply.github.com>
2023-11-12 13:23:05 +0600

    Explicit & Union Types define

--- output/script.js
+++ output/script.js
@@ -1,2 +1,7 @@
 "use strict";
-console.log("Bangladesh");
+let a;
+// a=4 invalid
+// a="" valid
+let b;
+// b="4" invalid
+// b=4 valid
--- src/script.ts
+++ src/script.ts
@@ -1,1 +1,7 @@
-console.log("Bangladesh")
+let a:string;
+// a=4 invalid
+// a="" valid
+
+let b:number;
+// b="4" invalid
+// b=4 valid
