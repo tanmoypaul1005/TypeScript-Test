fa4b25f6862b00f940f4a67046115e30f077f3d0
Tanmoy paul <84624966+tanmoypaul1005@users.noreply.github.com>
2023-11-12 15:58:59 +0600

    type aliases is done

--- output/script.js
+++ output/script.js
@@ -1,5 +1,7 @@
 "use strict";
 const userDetails = (id, details) => {
+    console.log(`user is id ${id} and details are ${details.name} and age ${details.age}`);
 };
 const seyHello = (id, details) => {
+    console.log(`user is id ${id} and details are ${details.name} and age ${details.age}`);
 };
--- src/script.ts
+++ src/script.ts
@@ -2,9 +2,9 @@ type stringOrNumber= string | number;
 type userType={name:string,age:number}
 
 const userDetails=(id:stringOrNumber,details:userType)=>{
-    
+    console.log(`user is id ${id} and details are ${details.name} and age ${details.age}`);
 }
 
 const seyHello=(id:stringOrNumber,details:userType)=>{
-    
+    console.log(`user is id ${id} and details are ${details.name} and age ${details.age}`);
 }
